bc5ce2821dc3893e56dddef2a511239349225156
SxxStar <2291874995@qq.com>
2023-06-06 19:40:44 +0800

    讲师列表、删除的显示、与后端接口交互

---
+++ src/api/teacher/teacher.js
@@ -0,0 +1,31 @@
+import request from '@/utils/request'
+export default {
+
+	/** 
+	 * 1 讲师列表(条件查询分页)
+	 * @current 当前页
+	 * @limit 每页记录数
+	 * @teacherQuery 条件对象
+	 */
+	getTeacherListPage(current,limit,teacherQuery) {
+		return request({
+			url: `/eduservice/teacher/pageTeacherCondition/${current}/${limit}`,
+			method: 'post',
+			// teacherQuery条件对象，后端使用RequestBody获取数据
+			// data表示把对象转换json进行传递到接口里面
+			data: teacherQuery,
+			//params: {}不可用于json数据
+		  })
+	},
+
+
+	/**
+	 * 2 删除讲师
+	 */
+	deleteTeacherId(id) {
+		return request({
+			url: `/eduservice/teacher/${id}`,
+			method: 'delete'
+		  })
+	}
+}
---
+++ src/views/edu/teacher/save/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+	  讲师添加
+	</div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>

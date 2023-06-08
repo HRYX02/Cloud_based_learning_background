fc9edbe89ccae3bdf2899d3c5d4263d604ce1aa0
SxxStar <2291874995@qq.com>
2023-06-08 16:07:35 +0800

    头像上传

--- src/views/edu/teacher/save/index.vue
+++ src/views/edu/teacher/save/index.vue
@@ -1,154 +1,201 @@
 <template>
-	<div class="app-container">
-		<el-form label-width="120px">
-			<el-form-item label="讲师名称">
-				<el-input v-model="teacher.name" />
-			</el-form-item>
-			<el-form-item label="讲师排序">
-				<el-input-number v-model="teacher.sort" controls-position="right" :min="0" />
-			</el-form-item>
-			<el-form-item label="讲师头衔">
-				<el-select v-model="teacher.level" clearable placeholder="请选择">
-					<el-option :value="1" label="高级讲师" />
-					<el-option :value="2" label="首席讲师" />
-				</el-select>
-			</el-form-item>
-			<el-form-item label="讲师资历">
-				<el-input v-model="teacher.career" />
-			</el-form-item>
-			<el-form-item label="讲师简介">
-				<el-input v-model="teacher.intro" :rows="10" type="textarea" />
-			</el-form-item>
+    <div class="app-container">
+        <el-form label-width="120px">
+            <el-form-item label="讲师名称">
+                <el-input v-model="teacher.name" />
+            </el-form-item>
+            <el-form-item label="讲师排序">
+                <el-input-number v-model="teacher.sort" controls-position="right" :min="0" />
+            </el-form-item>
+            <el-form-item label="讲师头衔">
+                <el-select v-model="teacher.level" clearable placeholder="请选择">
+                    <el-option :value="1" label="高级讲师" />
+                    <el-option :value="2" label="首席讲师" />
+                </el-select>
+            </el-form-item>
+            <el-form-item label="讲师资历">
+                <el-input v-model="teacher.career" />
+            </el-form-item>
+            <el-form-item label="讲师简介">
+                <el-input v-model="teacher.intro" :rows="10" type="textarea" />
+            </el-form-item>
 
-			<!-- 讲师头像：TODO -->
+            <!-- 讲师头像 -->
+            <el-form-item label="讲师头像">
 
-			<el-form-item>
-				<el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
-			</el-form-item>
-		</el-form>
-	</div>
+                <!-- 头衔缩略图 -->
+                <pan-thumb :image="teacher.avatar" />
+                <!-- 文件上传按钮 -->
+                <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow = true">更换头像
+                </el-button>
+
+                <!--
+                    v-show：是否显示上传组件
+                    :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+                    :url：后台上传的url地址
+                    @close：关闭上传组件
+                    @crop-upload-success：上传成功后的回调
+                -->
+                <image-cropper v-show="imagecropperShow" :width="300" :height="300" :key="imagecropperKey"
+                    :url="'/eduoss/fileoss'" field="file" @close="close" @crop-upload-success="cropSuccess" />
+            </el-form-item>
+
+            <el-form-item>
+                <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
   
 <script>
 import teacherApi from '@/api/edu/teacher/teacher';
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
 export default {
-	name: 'Save',
-	data() {
-		return {
-			teacher: {
-				name: '',
-				sort: 0,
-				level: 1,
-				career: '',
-				intro: '',
-				avatar: ''
-			},
-			saveBtnDisabled: false  // 保存按钮是否禁用,
-		}
-	},
-	// 页面渲染之前执行
-	created() {
-		this.init()
-	},
+    name: 'Save',
+    components: { ImageCropper, PanThumb },
+    data() {
+        return {
+            teacher: {
+                name: '',
+                sort: 0,
+                level: 1,
+                career: '',
+                intro: '',
+                avatar: '',
+            },
 
-	watch: {  //监听
-		$route(to, from) { //路由变化方式，路由发生变化，方法就会执行
-			this.init()
-		}
-	},
+            // 上传弹框组件是否显示
+            imagecropperShow: false,
+            // 上传组件key的值
+            imagecropperKey: 0,
+            // 保存按钮是否禁用,
+            saveBtnDisabled: false
+        }
+    },
+    // 页面渲染之前执行
+    created() {
+        this.init()
+    },
 
-	methods: {
+    watch: {  //监听
+        $route(to, from) { //路由变化方式，路由发生变化，方法就会执行
+            this.init()
+        }
+    },
 
-		/**
-		 * @description 根据ID进行查询对数据进行回显
-		 * @author SxxStar
-		 */
-		init() {
-			//判断路径有id值,做修改
-			if (this.$route.params && this.$route.params.id) {
-				//从路径获取id值
-				const id = this.$route.params.id
-				//调用根据id查询的方法
-				this.getInfo(id)
-			} else { //路径没有id值，做添加
-				//清空表单
-				this.teacher = {}
-			}
-		},
+    methods: {
 
-		/**
-		 * @description 判断添加或更改讲师
-		 * @author SxxStar
-		 */
-		saveOrUpdate() {
-			if (!this.teacher.id) {
-				// 添加
-				this.saveTeacher()
-			} else {
-				// 修改
-				this.updateTeacher()
-			}
-		},
+        /**
+         * @description 根据ID进行查询对数据进行回显
+         * @author SxxStar
+         */
+        init() {
+            //判断路径有id值,做修改
+            if (this.$route.params && this.$route.params.id) {
+                //从路径获取id值
+                const id = this.$route.params.id
+                //调用根据id查询的方法
+                this.getInfo(id)
+            } else { //路径没有id值，做添加
+                //清空表单
+                this.teacher = {}
+            }
+        },
 
-		/**
-		 * @description 添加讲师
-		 * @author SxxStar
-		 */
-		saveTeacher() {
-			teacherApi.addTeacher(this.teacher)
-				// 添加成功
-				.then(response => {
-					// 提示信息
-					this.$message({
-						type: 'success',
-						message: '讲师添加成功'
-					});
-					// 回到列表页面 路由跳转
-					this.$router.push({ path: '/teacher/table' })
-				})
-				.catch(error => {
-					// 提示信息
-					this.$message.error('讲师添加失败');
-				})
-		},
+        /**
+         * @description 判断添加或更改讲师
+         * @author SxxStar
+         */
+        saveOrUpdate() {
+            if (!this.teacher.id) {
+                // 添加
+                this.saveTeacher()
+            } else {
+                // 修改
+                this.updateTeacher()
+            }
+        },
 
-		/**
-		 * @description 根据讲师ID查询数据
-		 * @author SxxStar
-		 */
-		getInfo(id) {
-			teacherApi.getTeacherInfo(id)
-				.then(response => {
-					this.teacher = response.data.teacher
-				})
-				.catch(error => {
+        /**
+         * @description 添加讲师
+         * @author SxxStar
+         */
+        saveTeacher() {
+            teacherApi.addTeacher(this.teacher)
+                // 添加成功
+                .then(response => {
+                    // 提示信息
+                    this.$message({
+                        type: 'success',
+                        message: '讲师添加成功'
+                    });
+                    // 回到列表页面 路由跳转
+                    this.$router.push({ path: '/teacher/table' })
+                })
+                .catch(error => {
+                    // 提示信息
+                    this.$message.error('讲师添加失败');
+                })
+        },
 
-				})
-		},
+        /**
+         * @description 根据讲师ID查询数据
+         * @author SxxStar
+         */
+        getInfo(id) {
+            teacherApi.getTeacherInfo(id)
+                .then(response => {
+                    this.teacher = response.data.teacher
+                })
+                .catch(error => {
 
-		/**
-		 * @description 修改讲师
-		 * @author SxxStar
-		 */
-		updateTeacher() {
-			teacherApi.updateTeacher(this.teacher)
-				// 修改成功
-				.then(response => {
-					// 提示信息
-					this.$message({
-						type: 'success',
-						message: '讲师修改成功'
-					});
-					// 回到列表页面 路由跳转
-					this.$router.push({ path: '/teacher/table' })
-				})
-				.catch(error => {
-					// 提示信息
-					this.$message.error('讲师修改失败');
-				})
-		}
+                })
+        },
 
-	}
+        /**
+         * @description 修改讲师
+         * @author SxxStar
+         */
+        updateTeacher() {
+            teacherApi.updateTeacher(this.teacher)
+                // 修改成功
+                .then(response => {
+                    // 提示信息
+                    this.$message({
+                        type: 'success',
+                        message: '讲师修改成功'
+                    });
+                    // 回到列表页面 路由跳转
+                    this.$router.push({ path: '/teacher/table' })
+                })
+                .catch(error => {
+                    // 提示信息
+                    this.$message.error('讲师修改失败');
+                })
+        },
+
+        /**
+         * @description 关闭上传弹框的方法
+         * @author SxxStar
+         */
+        close() {
+            this.imagecropperShow = false
+            this.imagecropperKey = this.imagecropperKey + 1
+        },
+
+        /**
+         * @description 上传成功后的方法
+         * @author SxxStar
+         */
+        cropSuccess(data) {
+            this.imagecropperShow = false
+            // 上传之后接口返回图片地址
+            this.teacher.avatar = data.url
+            this.imagecropperKey = this.imagecropperKey + 1
+        }
+
+    }
 
 }
 </script>
